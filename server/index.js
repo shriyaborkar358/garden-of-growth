7316211e17fbaea55ab7b600dcc4b8082504d66f
unknown <shriyaborkar71@gmail.com>
2024-09-04 15:23:16 +0530

    Initial setup

---
+++ server/index.js
@@ -0,0 +1,32 @@
+import express from "express"
+import cors from "cors"
+import mongoose from "mongoose"
+import dotenv, { config } from "dotenv"
+dotenv.config()
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+const connectDB = async () => {
+    const conn = await mongoose.connect(process.env.MONGO_URI)
+
+    if(conn){
+        console.log(`MongoDB connected successfully ✅`);
+        
+    }
+}
+connectDB()
+
+app.get("/", (req, res)=>{
+    res.json({
+        message : "Welc0me to Nursery Project",
+        success: true
+    })
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
